081d982f4d25e934b23c1bc7a63560809f84699e
AurelOconnell <aurelienoconnell@gmail.com>
2023-09-27 14:13:40 +0200

    test

---
+++ service_angular/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CocktailService } from './cocktail.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'service';
+
+  constructor(public cocktailService: CocktailService) {
+
+  }
+}
---
+++ service_angular/src/app/cocktail-list/cocktail-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CocktailService } from '../cocktail.service';
+
+@Component({
+  selector: 'app-cocktail-list',
+  templateUrl: './cocktail-list.component.html',
+  styleUrls: ['./cocktail-list.component.css']
+})
+export class CocktailListComponent {
+
+  constructor(public cocktailService: CocktailService) {
+    
+  }
+    cocktails = this.cocktailService.getCocktails();
+}
